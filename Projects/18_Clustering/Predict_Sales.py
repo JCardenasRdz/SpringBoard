f6764c05ca952d41f477da3e0bc81765a14278e6
Julio Cardenas <cardenaj@email.arizona.edu>
2017-03-11 07:39:38 -0700

    My final version

--- Projects/18_Clustering/Predict_Sales.py
+++ Projects/18_Clustering/Predict_Sales.py
@@ -4,12 +4,11 @@ df_offers = pd.read_excel("./WineKMC.xlsx", sheetname=0)
 df_offers.columns = ["offer_id", "campaign", "varietal", "min_qty", "discount", "origin", "past_peak"]
 
 # import data for transactions
-df_transactions = pd.read_excel("./WineKMC.xlsx", sheetname=1)
-df_transactions.columns = ["customer_name", "offer_id"]
+df_transactions = pd.read_excel("./WineKMC.xlsx", sheetname=1,names=["customer_name", "offer_id"])
 df_transactions['n'] = 1
 
 # estimate total sales per offer_id
-df_total_sales = pd.merge(df_offers['offer_id'].to_frame(), df_transactions)
+df_total_sales = pd.merge(df_offers[['offer_id']], df_transactions)
 df_total_sales = df_total_sales.groupby(['offer_id']).sum()
 df_total_sales.columns = ['Total_Sales']
 df_total_sales = df_total_sales.reset_index()
@@ -23,3 +22,6 @@ y = df[['Total_Sales']]
 Xnod = df[df.columns.difference(['Total_Sales','offer_id'])]
 X = pd.get_dummies(Xnod, prefix=['cam', 'var','ori'])
 X = X.astype(float)
+
+#import statsmodels.api as sm
+#est = sm.OLS(y, X).fit(); est.summary()
